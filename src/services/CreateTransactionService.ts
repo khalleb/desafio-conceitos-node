d1df5220fa653dd5c1f3f38b1c0d96cdfb8fec31
khalleb <khallebw@gmail.com>
2020-09-02 23:26:24 -0300

    Finalizado desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,13 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if(type ==='outcome' && total < value){
+      throw Error('O valor de retirada nao pode ser maior que o valor atual do caixa');
+    }
+    const appointment = this.transactionsRepository.create({ title, value, type });
+    return appointment;
   }
 }
 
